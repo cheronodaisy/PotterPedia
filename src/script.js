6a82ba01d4220ae64f0be3a2ac721c18fb168e1e
Daisy Cherono <85867696+cheronodaisy@users.noreply.github.com>
2023-11-14 09:56:46 +0300

    Add comments

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
+// Get references to HTML elements
 const characterListElement = document.getElementById("character-list");
 const characterDetailsElement = document.getElementById("character-details");
 const searchInput = document.getElementById("search-input");
 
+// Function to display a list of characters
 function displayCharacterList(characters) {
-  characterListElement.innerHTML = "";
+  characterListElement.innerHTML = ""; // Clear previous content
+
+  // Create a card element for each character
   characters.forEach((character) => {
     const card = document.createElement("div");
     card.classList.add("card");
@@ -11,6 +15,7 @@ function displayCharacterList(characters) {
       character.dateOfBirth || "Unknown"
     })`;
 
+    // Add click event listener to display details when a card is clicked
     card.addEventListener("click", () => {
       displayCharacterDetails(character);
     });
@@ -19,6 +24,7 @@ function displayCharacterList(characters) {
   });
 }
 
+// Function to display detailed information about a character
 function displayCharacterDetails(character) {
   characterDetailsElement.innerHTML = `
         <h2>${character.name}</h2>
@@ -29,7 +35,7 @@ function displayCharacterDetails(character) {
         <p>Species: ${character.species || "Unknown"}</p>
     `;
 }
-
+// Function to search for characters based on the input query
 function searchCharacters(query) {
   fetch(`https://hp-api.onrender.com/api/characters?name=${query}`)
     .then((response) => response.json())
@@ -40,12 +46,14 @@ function searchCharacters(query) {
       console.error("Error:", error);
     });
 }
-
+// Event listener for input changes in the search bar
 searchInput.addEventListener("input", (event) => {
   const query = event.target.value.trim();
+  // Perform a search when the query is long enough
   if (query.length >= 3) {
     searchCharacters(query);
   } else {
+    // Clear the character list and details if the query is too short
     characterListElement.innerHTML = "";
     characterDetailsElement.innerHTML = "";
   }
